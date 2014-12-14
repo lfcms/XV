dc5112daeb9168dcd8ae36f7479d61d4303ea7db
Michael Wight <mwight90@gmail.com>
2014-12-14 15:17:45 -0500

    init

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+		<title>%title%</title>
+		<link href="http://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" type="text/css" href="%skinbase%/css/styles.css" />
+	</head>
+	<body>
+		<header class="dark_gray light text-center">
+			<div class="container">
+				<h1>Fresh Framework</h1>
+				<div class="row">
+					<div class="col-4"></div>
+					<div class="col-4">
+						<a class="blue button" href="http://littlefootcms.com/files/download/littlefoot.zip" id="download_littlefoot">Download Littlefoot</a>
+					</div>
+					<div class="col-4"></div>
+				</div>
+			</div>
+		</header>
+		<nav>
+			<div class="container">
+				<div class="row">
+					<div class="col-12 pull-right">
+						%nav%
+						%login%
+					</div>
+				</div>
+			</div>
+		</nav>
+		<div class="container">
+			%content%
+		</div>
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-12">
+						Powered by &copy<a href="http://littlefootcms.com">LittlefootCMS</a>
+					<div>
+				</div>
+			</div>
+		</footer>
+	</body>
+</html>
